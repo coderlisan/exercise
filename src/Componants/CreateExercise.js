1947f6b41fa177b9c0c2f43ddf8d3408e24ecf23
Ash0011 <md_ashraful@icloud.com>
2023-01-18 09:46:06 +0930

    Update CreateExercise.js
    changed input to a button for semantic html. removed onSubmit because I was sending data manually by postUserData through onClick function.

--- src/Componants/CreateExercise.js
+++ src/Componants/CreateExercise.js
@@ -7,26 +7,33 @@ export default function CreateExercise() {
 
   const postUserData = e => {
     e.preventDefault()
-    let user = userdata
+    let user = {username: userdata}
 
-    axios.post('http://localhost:5000/users/add', user).then(res => {
+    axios.post('http://localhost:5000/users/add', user, {
+    headers:{
+    "Accept": "application/json",
+    "Content-Type": "application/json;charset=UTF-8"
+    }}).then(res => {
       console.log(res.data)
       setTimeout(() => {
         setUserdata('')
       }, 1000)
-    }).catch(err => { console.log('From axios', err) })
+    }).catch(
+      (e) => { 
+        console.log('From axios', e) }
+    )
   }
 
   return (
     <div>
       <h3>Create new User</h3>
-      <form onSubmit={postUserData}>
+      <form>
         <div className='form-group'>
           <label>Username: </label>
           <input type='text' className='form-control' value={userdata} onChange={handleChange} />
         </div>
         <div className="form-group">
-          <input type='submit' value='Create' className='btn btn-primary' />
+                    <button onClick={()=>postUserData} className='btn btn-primary'> Send data </button> 
         </div>
       </form>
     </div>
