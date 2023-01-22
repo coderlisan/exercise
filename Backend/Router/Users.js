08607bf2b6d238e80cce747c371c766ef368a2e9
coderlisan <coderlisan@gmail.com>
2023-01-22 12:23:56 +0600

    user এর মাধ্যমে সফল ভাবে ডাটাকে Backend এ পাঠালাম

--- Backend/Router/Users.js
+++ Backend/Router/Users.js
@@ -1,6 +1,4 @@
 const { Router } = require('express')
-// const router = require('express').Router()
-// let User = require('../Schema/User.js')
 const USERSCHEMA = require('../Schema/User.js')
 
 const userRouter = Router()
@@ -11,25 +9,17 @@ userRouter.route('/add').post((req, res) => {
   const username = req.body
   console.log('It is username - - - ', username)
 
-  // const newUser = new User({ username })
-  // newUser.save().then(() => {
-  //   res.status(200).json('added')
-  // }).catch(err => res.status(400).json('Error' + err))
-  USERSCHEMA.create(username, (err, data)=>{
-    if (err){
-      console.log('Error message', err.message)
-      res.status(400).json('Error', err)
-  } else{
-    res.status(200).json('Received Data', data)
-  }
+  USERSCHEMA.create(username, (error, data) => {
+    if (error) {
+      console.log('Error message', error.message)
+      res.status(400).json({ "Error": error.message })
+    } else {
+      res.status(200).json({ "Saved to database": data })
+    }
   })
 })
 
-userRouter.route('/search').get((req, res) => {
-  User.find().then(users => res.json(users)).catch(err => res.status(400).json('Error' + err))
-})
-
-userRouter.route('/').get((req, res)=>{
+userRouter.route('/').get((req, res) => {
   res.status(200).json('Database found')
 })
 
--- src/Componants/CreateExercise.js
+++ src/Componants/CreateExercise.js
@@ -7,26 +7,32 @@ export default function CreateExercise() {
 
   const postUserData = e => {
     e.preventDefault()
-    let user = userdata
+    console.log("called")
+    let user = { username: userdata }
 
-    axios.post('http://localhost:5000/users/add', user).then(res => {
-      console.log(res.data)
+    axios.post('http://localhost:5000/users/add', user, {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json;charset=UTF-8"
+      }
+    }).then(res => {
+      console.log('Response -------', res.data)
       setTimeout(() => {
         setUserdata('')
       }, 1000)
-    }).catch(err => { console.log('From axios', err) })
+    }).catch(err => console.log('From axios', err))
   }
 
   return (
     <div>
       <h3>Create new User</h3>
-      <form onSubmit={postUserData}>
+      <form>
         <div className='form-group'>
           <label>Username: </label>
           <input type='text' className='form-control' value={userdata} onChange={handleChange} />
         </div>
         <div className="form-group">
-          <input type='submit' value='Create' className='btn btn-primary' />
+          <button onClick={postUserData} className='btn btn-primary'>Send Data</button>
         </div>
       </form>
     </div>
